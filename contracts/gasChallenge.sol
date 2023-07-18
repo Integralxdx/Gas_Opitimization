d06ddc6142cf28b4bcba5d9618c3940d95621561
Integralxdx <97835332+Integralxdx@users.noreply.github.com>
2023-07-18 21:17:47 +0100

    Updated file

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -2,34 +2,39 @@
 pragma solidity ^0.8.0;
 
 contract gasChallenge {
-    uint[10] numbers = [1,2,3,4,5,6,7,8,9,10]; // 1. Fixed-size array
+    //Implement Fixed-Size Array Technique Here
+    // 1. Fixed-size array
+    uint[10] numbers = [1,2,3,4,5,6,7,8,9,10]; 
 
-    uint256 cachedSum; // 2. Cached state variable
-
-    function getSumOfArray() public view returns (uint256) {
-        uint256 sum = 0;
-        for (uint256 i = 0; i < numbers.length; i++) {
+    //Function to check for sum of array
+    //No changes required in this function
+    function getSumOfArray() public view returns (uint) {
+        uint sum = 0;
+        for (uint i = 0; i < numbers.length; i++) {
             sum += numbers[i];
         }
         return sum;
     }
 
     function notOptimizedFunction() public {
-        for (uint256 i = 0; i < numbers.length; i++) {
+        for (uint i = 0; i < numbers.length; i++) {
             numbers[i] = 0;
         }
     }
 
+    //Implement Remaining Gas Optimization Techniques Here
+    //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-        unchecked { // 3. Unchecked block
-            for (uint256 i = 0; i < numbers.length; i++) { // 4. Different for loop increment syntax
+        // 2. Cached state variable
+        uint cache = numbers.length; 
+        // 3. Unchecked block
+        unchecked { 
+        // 4. Different for loop increment syntax
+            for (uint i = 0; i < cache; i++ ) { 
                 numbers[i] = 0;
+                }
             }
         }
-        cachedSum = 0; // Cache the sum of the array
     }
 
-    function getCachedSum() public view returns (uint256) {
-        return cachedSum;
-    }
-}
+
